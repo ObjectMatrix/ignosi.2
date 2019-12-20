0bba276ab91c0363b158f0b51bee6c9589fadf3d
Ahm Asaduzzaman <Ahm.Asaduzzaman@libertymutual.com>
2019-12-20 14:34:04 -0500

    added orm for answer

--- app/api/answer.js
+++ app/api/answer.js
@@ -1,5 +1,13 @@
 module.exports = (app, db) => {
-  app.get('/answer', (req, res) => {
-    db.astabanswerbank.findAll().then((Answer) => res.json(Answer));
+  app.get('/answer/question/:id', (req, res) => {
+    db.astabanswerbank.findAll({
+      where: { abQuestionId: `${req.params.id}` },
+    }).then((Answer) => res.json(Answer));
+  })
+
+  app.get('/answer/lesson/:lesson', (req, res) => {
+    db.astabanswerbank.findAndCountAll({
+      where: { abLessonName: `${req.params.lesson}` },
+    }).then((Answer) => res.json(Answer));
   })
 }
--- app/api/passage.js
+++ app/api/passage.js
@@ -2,4 +2,13 @@ module.exports = (app, db) => {
   app.get('/passage', (req, res) => {
     db.astabpassagebank.findAll().then((Passage) => res.json(Passage));
   })
+
+  app.get('/passage/lesson/:lesson', (req, res) => {
+    db.astabpassagebank.findAndCountAll({
+      where: { pbLessonName: `${req.params.lesson}` },
+      order: [['pbSequencer', 'ASC']],
+    }).then((Passage) => res.json(Passage));
+  })
+
+  app.get('/passage/:id', (req, res) => db.astabpassagebank.findByPk(req.params.id).then((result) => res.json(result)));
 }
